742079f328abe569bbe00bdd0599f246c389835c
301prateek <prateek.patil301@gmail.com>
2020-10-19 11:34:07 +0530

    Added: Code for dice game

---
+++ diceGame.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+
+dice1=$((RANDOM%6+1))
+dice2=$((RANDOM%6+1))
+rolls=0
+sum=0
+
+while(true)
+do
+	dice1=$((RANDOM%6+1))
+	dice2=$((RANDOM%6+1))
+	if [ $dice1 -eq 6 -a $dice2 -eq 6 ]
+	then
+		break
+	elif [ $dice1 -lt 6 -a $dice2 -lt 6 ]
+	then
+		rolls=$(($rolls + 1))
+		sum=$(($sum + $dice1 + $dice2))
+	fi
+done
+
+echo "Sum = $sum"
+echo "Total Rolls= $rolls"
